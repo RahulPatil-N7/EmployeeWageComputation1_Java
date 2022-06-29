e10f2d0ba60da83f9f99d303bcbaa1731a620798
RahulPatil-N7 <rahul.patil.3125@gmail.com>
2022-06-29 23:31:06 +0530

    Added :Welcome message.

---
+++ EmployeeWageComputation1/src/main/java/com/bridgelabz/EmployeeWage/EmployeeWageMain.java
@@ -0,0 +1,9 @@
+package com.bridgelabz.EmployeeWage;
+
+public class EmployeeWageMain {
+
+	public static void main(String[] args) {
+		//printing Welcome message
+		System.out.println("Welcome To Employee Wage Computation Program.");
+}
+}
