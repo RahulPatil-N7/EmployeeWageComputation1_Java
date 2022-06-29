c250ffcaf776099205223d591cc3efd38f765658
RahulPatil-N7 <rahul.patil.3125@gmail.com>
2022-06-29 23:43:07 +0530

    Updated :Calculate employee daily wage.

--- EmployeeWageComputation1/src/main/java/com/bridgelabz/EmployeeWage/EmployeeWageMain.java
+++ EmployeeWageComputation1/src/main/java/com/bridgelabz/EmployeeWage/EmployeeWageMain.java
@@ -5,17 +5,26 @@ public class EmployeeWageMain {
 	public static void main(String[] args) {
 		//printing Welcome message
 		System.out.println("Welcome To Employee Wage Computation Program.");
-        int isFullTime = 1;
-        //Using random function to check attendance
+		int isFullTime = 1;
+		int wagePerHour = 20;
+		int workingHrs = 0;
+		int dailyWage = 0;
+		//Using random() to check attendance
 		double empCheck = Math.floor(Math.random() * 10) % 2;
 		if(empCheck == isFullTime)
-		{
-			System.out.println("Employee is Present.");
-		}
+		{			
+			workingHrs = 8;
+		System.out.println("Employee is Present.");
+        }
 		else
-		{
-			System.out.println("Employee is Absent.");
-		}
-
-	}
+		{	
+			workingHrs = 0;
+		System.out.println("Employee is Absent.");
+		}		
+		dailyWage = workingHrs * wagePerHour;
+		//printing daily wage
+		System.out.println("Employee Daily Wage is: " + dailyWage);
+       }
 }
+
+
