cdc0d94b829c86569967f21c6d35e534b0299583
RahulPatil-N7 <rahul.patil.3125@gmail.com>
2022-06-29 23:36:22 +0530

    Updated :Check employee attendance.

--- EmployeeWageComputation1/src/main/java/com/bridgelabz/EmployeeWage/EmployeeWageMain.java
+++ EmployeeWageComputation1/src/main/java/com/bridgelabz/EmployeeWage/EmployeeWageMain.java
@@ -5,5 +5,17 @@ public class EmployeeWageMain {
 	public static void main(String[] args) {
 		//printing Welcome message
 		System.out.println("Welcome To Employee Wage Computation Program.");
-}
+        int isFullTime = 1;
+        //Using random function to check attendance
+		double empCheck = Math.floor(Math.random() * 10) % 2;
+		if(empCheck == isFullTime)
+		{
+			System.out.println("Employee is Present.");
+		}
+		else
+		{
+			System.out.println("Employee is Absent.");
+		}
+
+	}
 }
