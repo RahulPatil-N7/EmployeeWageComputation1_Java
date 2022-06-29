22f99063bf6dd7a737cdf9c8fdb557b019e07f8b
RahulPatil-N7 <rahul.patil.3125@gmail.com>
2022-06-29 23:47:06 +0530

    Updated :Part time employee wage added.

--- EmployeeWageComputation1/src/main/java/com/bridgelabz/EmployeeWage/EmployeeWageMain.java
+++ EmployeeWageComputation1/src/main/java/com/bridgelabz/EmployeeWage/EmployeeWageMain.java
@@ -5,25 +5,32 @@ public class EmployeeWageMain {
 	public static void main(String[] args) {
 		//printing Welcome message
 		System.out.println("Welcome To Employee Wage Computation Program.");
-		int isFullTime = 1;
+		int isPartTime = 1;
+		int isFullTime = 2;
 		int wagePerHour = 20;
 		int workingHrs = 0;
 		int dailyWage = 0;
 		//Using random() to check attendance
-		double empCheck = Math.floor(Math.random() * 10) % 2;
-		if(empCheck == isFullTime)
-		{			
+		double empCheck = Math.floor(Math.random() * 10) % 3;
+		if(empCheck == isPartTime)
+		{
+			//part time employee wage
+			workingHrs = 4;
+		System.out.println("Employee is Present for "+workingHrs+" Hrs.");
+		}
+		else if(empCheck == isFullTime)
+		{
 			workingHrs = 8;
-		System.out.println("Employee is Present.");
-        }
+		System.out.println("Employee is Present for "+workingHrs+" Hrs.");
+		}
 		else
 		{	
 			workingHrs = 0;
-		System.out.println("Employee is Absent.");
-		}		
+		System.out.println("Employee is Absent");
+		}
 		dailyWage = workingHrs * wagePerHour;
 		//printing daily wage
-		System.out.println("Employee Daily Wage is: " + dailyWage);
+		System.out.println("Daily Wage is : " + dailyWage);
        }
 }
 
