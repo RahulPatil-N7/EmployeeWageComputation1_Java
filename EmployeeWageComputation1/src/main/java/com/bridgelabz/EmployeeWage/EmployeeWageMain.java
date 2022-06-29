5337bba139dcd7d0fd62de9d64a8f1fbfd074920
RahulPatil-N7 <rahul.patil.3125@gmail.com>
2022-06-29 23:56:53 +0530

    Updated :Employee monthly wage added.

--- EmployeeWageComputation1/src/main/java/com/bridgelabz/EmployeeWage/EmployeeWageMain.java
+++ EmployeeWageComputation1/src/main/java/com/bridgelabz/EmployeeWage/EmployeeWageMain.java
@@ -4,11 +4,14 @@ public class EmployeeWageMain {
     public static final int isPartTime = 1;
 	public static final int isFullTime = 2;
 	public static final int wagePerHour = 20;
+	public static final int workingDays = 20;
     public static void main(String[] args) {
 		//printing welcome message
 		System.out.println("Welcome To Employee Wage Compuatation Program.");
 		int workingHrs = 0;
-		int dailyWage = 0;
+		int dailyWage = 0,monthlyWage=0;
+		for(int day=0;day<workingDays;day++)
+		{
 		//using random() to check attendance 
 		int empCheck = (int) (Math.floor(Math.random() * 10) % 3);
 		//Using Switch case
@@ -27,8 +30,11 @@ public class EmployeeWageMain {
 		}
 		//printing daily wage
 		dailyWage = workingHrs * wagePerHour;
+		monthlyWage += dailyWage;
 		System.out.println("Daily Wage is : " + dailyWage);
        }
+		//printing monthly wage
+		System.out.println("\nMonthly Wage is: " + monthlyWage);
 }
-
+}
 
